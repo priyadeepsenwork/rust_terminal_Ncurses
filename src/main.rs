cc2b1ada7203f5fa8e66dfd413dc2d6d29a220ab
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-08-06 21:35:21 +0530

    cursors working properly(not fully checked)

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
-extern crate ncurses;
-
 //* we can also use*/
 use ncurses::*;
+use std::cmp::*;
+
+//? CONSTANTS
+const REGULAR_PAIR: i16 = 1;
+const HIGHLIGHT_PAIR: i16 = 2;
 
 fn main() {
-    //* start(initialize) ncurses
+    //* start(initialize) ncurses, no-echo and no-cursor
     initscr();
+    noecho();
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
+    //? Colors
+    start_color();
+    init_pair(REGULAR_PAIR, COLOR_WHITE, COLOR_BLACK);
+    init_pair(HIGHLIGHT_PAIR, COLOR_BLACK, COLOR_WHITE);
+    
+    // program
     let mut quit = false;
-    let todos = vec!["Write a Todo App", "Buy a bread", "Get a Job", "Fix bugs"];
+    let todos = vec!["Write a Todo App", "Buy a bread", "Get a Job"];
 
-    //* EVENT LOOP */
+    let mut todo_curr: usize = 0;
+
     while !quit {
-        for (row, todo) in todos.iter().enumerate() {
-            mv(row as i32, 0);
+        for (index, todo) in todos.iter().enumerate() {
+            let pair = {
+                if todo_curr == index {
+                    HIGHLIGHT_PAIR
+                } else {
+                    REGULAR_PAIR
+                }
+            };
+
+            attron(COLOR_PAIR(pair));
+            mv(index as i32, 1);
             addstr(*todo);
+            attroff(COLOR_PAIR(pair));
         }
 
         //refresh here (as per me)
@@ -23,12 +45,19 @@ fn main() {
         let key = getch();
         match key as u8 as char {
             'q' => quit = true,
+            'w' => {
+                if todo_curr > 0 {
+                    todo_curr -= 1;
+                }
+            },
+            's' => {
+                todo_curr = min(todo_curr + 1, todos.len()  - 1);
+            },
             _ => {}
         }
     }
 
-    //wait for a keypress
-    //getch(); <--- Not required, exit when types 'q' instantly
+ 
 
     //* end ncurses window
     endwin();
