909b15b67ccd8c169a3d3b4e841c0c0f9bfbb042
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-08-06 19:38:06 +0530

    slowly building stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate ncurses;
+
+//* we can also use*/
+// use ncurses::*
+
+//better use this with autocomplete IDE
+use ncurses::{addstr, endwin, getch, initscr, refresh};
+
 fn main() {
-    println!("Hello World!")
+    //* start ncurses
+    initscr();
+    
+    //add string
+    addstr("Hello, World!").unwrap();
+    
+    //refresh screen 
+    refresh();
+
+    let mut quit = false;
+
+    while !quit {
+        let key = getch();
+
+        match key as u8 as char {
+            'q' => quit = true,
+            _ => {}
+        }
+    }
+    
+    //wait for a keypress
+    //getch(); <--- Not required, exit when types 'q' instantly
+
+    //* end ncurses window
+    endwin();
 }
